4da09f4c7be18b30bbdbafdc22a37c73165490c5
Dorota Jarecka <djarecka@gmail.com>
2014-07-24 23:30:32 -0600

    small chnages, todo

--- analytic_blk_1m_pytest.py
+++ analytic_blk_1m_pytest.py
@@ -23,7 +23,7 @@ def pottemp_dry(rv, press, T):
     return T * (p0 / press * (1. + rv * Rv/Rd))**(Rd/cp)
 
                 
-
+#TODO a co jesli jest rc i rv<rsat?? DODAC!!
 #linearization of: r_v - \delta r_v = r_vs(\theta + \delta \theta) and
 # \delta \theta = L/c_p * \theta/T 
 def delta_r(rv, T, p, es_ref=es0, T_ref=T0):
@@ -31,9 +31,9 @@ def delta_r(rv, T, p, es_ref=es0, T_ref=T0):
     return (rv - rvs) / (1. + rvs / T**2 * L**2 / Rv / cp)
     
 if __name__ == "__main__":
-    print mixrat_sat(283.15, 900.e2)
-    print pot_temp(283.15, 900.e2)
-    print press_sat(283.15, 1227., 283.15)
+#    print mixrat_sat(283.15, 900.e2)
+#    print pot_temp(283.15, 900.e2)
+#    print press_sat(283.15, 1227., 283.15)
     print delta_r(10.e-3, 283.15, 900.e2)
-    print delta_r(8.e-3, 283.15, 900.e2)
-    print delta_r(9.e-3, 283.15, 900.e2)
+#    print delta_r(8.e-3, 283.15, 900.e2)
+#    print delta_r(9.e-3, 283.15, 900.e2)
