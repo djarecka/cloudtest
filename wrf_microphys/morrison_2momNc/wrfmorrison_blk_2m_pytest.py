a9c166ac5630da1ccc95ee8f9f7762259513ca5b
djarecka <djarecka@gmail.com>
2014-10-20 16:46:48 -0600

    coming back to the version that uses Morrison scheme with Nc

--- wrf_microphys/morrison_2momNc/wrfmorrison_blk_2m_pytest.py
+++ wrf_microphys/morrison_2momNc/wrfmorrison_blk_2m_pytest.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append(".")
 
 import numpy as np
-from cffi_morrison_2mom import morrison_2mom_simplewarm
+from cffi_morrison_2momNc import morrison_2momNc_simplewarm
 import pdb
 
 nx = 1
@@ -12,7 +12,7 @@ dz = np.ones((nx,nz,ny)) * 20.
 
 def adj_cellwise(press_in, T_in, qv_in, qc_in, nc_in, qr_in, nr_in, dt):
     print "wrfmorrison qv_przed", qv_in, qc_in, nc_in, press_in, T_in
-    morrison_2mom_simplewarm(nx, ny, nz, qc_in, qr_in, nc_in, nr_in,
+    morrison_2mom_simplewarmNc(nx, ny, nz, qc_in, qr_in, nc_in, nr_in,
                                T_in, qv_in, press_in, dz, dt)
     print "wrfmorrison qv po", qv_in, qc_in, qr_in, T_in
     return qv_in, qc_in, nc_in, qr_in, nr_in
