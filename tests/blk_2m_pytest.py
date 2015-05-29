c9d8514bbabde98a39ac178afceb62fd3ab14762
djarecka <djarecka@gmail.com>
2015-05-29 14:24:52 -0600

    using analytical solution in blk_2m_pytest (in analogy to blk_1m...)

--- tests/blk_2m_pytest.py
+++ tests/blk_2m_pytest.py
@@ -1,10 +1,12 @@
 import sys
 sys.path.append(".")
+sys.path.append("../../")
 sys.path.append("wrf_microphys/morrison_2momNc/")
 import pytest
 import inspect
 import pdb
 import numpy as np
+import analytic_blk_1m_pytest as an_blk
 
 # typical values as an example
 press_0 = np.array([900.e2  ])
@@ -37,6 +39,17 @@ def condensation(lib, press = None, T = None, rv = None,
     rv, rc, nc, rr, nr = lib_adj.adj_cellwise(press, T, rv, rc, nc, rr, nr, dt)
     return rv, rc
 
+def analytic_condensation(press = None, T = None, rv = None, rc = None):
+     T = T if T!=None else T_0.copy()
+     rv = rv if rv!=None else rv_0.copy()
+     rc = rc if rc!=None else rc_0.copy()
+     press = press if press!=None else press_0.copy()
+     r_an = {}
+     #calling analytical function calculating del_r for supersaturation adjustment
+     del_r = an_blk.delta_r(rv, rc, T, press)
+     r_an["rv"] = rv - del_r
+     r_an["rc"] = rc + del_r
+     return r_an
 
 @pytest.mark.skipif
 @pytest.mark.parametrize("arg", [
@@ -52,30 +65,22 @@ def test_exeptions_wrongvalue(libname, arg):
     #the line below can give you information about the exception 
     #print "exception info:", excinfo.value
     
-
-#TODO: wypisac znane outputy, moze tez theta?
-#TODO: polaczyc z plikiem analytic_blk_1m_pytest??
 #@pytest.mark.skipif
-@pytest.mark.parametrize("arg, expected", [
-        ({"rv" :  np.array([0.]),      "rc" :  np.array([0.])},
-         {"rv" :  np.array([0.]),      "rc" :  np.array([0.])}), # no water
-        ({"rv" :  np.array([7.e-3]),   "rc" :  np.array([0.])},
-         {"rv" :  np.array([7.e-3]),   "rc" :  np.array([0.])}), # no cl water and subsat.
-        ({"rv" :  np.array([10.e-3]),  "rc" :  np.array([0.])},
-         {"rv" :  np.array([9.44e-3]), "rc" :  np.array([.56e-3])}), # no cl water and supersat.
-        ({"rv" :  np.array([5.e-3]),   "rc" :  np.array([1.e-3])},
-         {"rv" :  np.array([6.e-3]),   "rc" :  np.array([0.])}), # subsat. leads to coplete evap.
-        ({"rv" :  np.array([8.e-3]),   "rc" :  np.array([1.e-3])},
-         {"rv" :  np.array([8.26e-3]), "rc" :  np.array([0.74e-3])}), # subsat. leads to some evap.
-        ({"rv" :  np.array([9.e-3]),   "rc" :  np.array([1.e-3])},
-         {"rv" :  np.array([8.85e-3]), "rc" :  np.array([1.15e-3])}), # supersat. leads to cond.
+@pytest.mark.parametrize("arg", [
+        ({"rv" :  np.array([0.]),      "rc" :  np.array([0.])}), # no water
+        ({"rv" :  np.array([7.e-3]),   "rc" :  np.array([0.])}), # no cl water and subsat.
+        ({"rv" :  np.array([10.e-3]),  "rc" :  np.array([0.])}), # no cl water and supersat.
+        ({"rv" :  np.array([5.e-3]),   "rc" :  np.array([1.e-3])}), # subsat. leads to coplete evap.
+        ({"rv" :  np.array([8.e-3]),   "rc" :  np.array([1.e-3])}), # subsat. leads to some evap.
+        ({"rv" :  np.array([9.e-3]),   "rc" :  np.array([1.e-3])}), # supersat. leads to cond.
     ])
 #TODO zastanowic sie nad epsilonem
-def test_expected_output_evapcond(libname, arg, expected, epsilon = 0.1):
+def test_expected_output_evapcond(libname, arg, epsilon = 0.1):
     print "\n in test_expected value before cond.", arg
+    r_an = analytic_condensation(**arg)
     rv, rc = condensation(lib=libname, **arg)
     #print "rv, rc po", rv, rc
-    for key, value in expected.items():
-        print "\n key, valuu, eval(key)", key, value, eval(key)
-        assert abs(eval(key) - value) <= epsilon * abs(value)
+    for key, value in arg.items():
+        print "\n key, valuu, eval(key)", key, value, r_an[key]
+        assert abs(value - r_an[key]) <= epsilon * abs(r_an[key])
 
