a279c8903ae0088dd3dd4c738a9e205147dac5c1
slayoo <slayoo@igf.fuw.edu.pl>
2015-07-29 09:28:10 +0200

    sd updates, fixing plotting of S

--- spurious_supersat/adv_hor.py
+++ spurious_supersat/adv_hor.py
@@ -1,18 +1,20 @@
 import sys
 sys.path.append(".")
 sys.path.append("../")
+
+import pdb
+
 import libmpdata
-import numpy as np
 import libcloudphxx as libcl
+import numpy as np
+import math
 #import analytic_blk_1m_pytest as an
 #from constants_pytest import Rd, Rv, cp, p0
-import pdb
 
 import matplotlib
 #matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
-import pdb
 
 def test():
     a = np.zeros((10,))
@@ -92,6 +94,7 @@ def libcl_spdr_init(rho_d, th_d, rv, C, dt, aerosol, dx=2): #TODO dx
     opts_init.dt = dt
     opts_init.nx = rv.shape[0]
     opts_init.dx = dx
+    opts_init.x1 = opts_init.nx * opts_init.dx
 
     def lognormal(lnr):
         from math import exp, log, sqrt, pi
@@ -110,32 +113,49 @@ def libcl_spdr_init(rho_d, th_d, rv, C, dt, aerosol, dx=2): #TODO dx
     micro.init(th_d, rv, rho_d, C_arr)
     return micro
 
-def libcl_spdr(rhod, thd, rv, dt, aerosol, micro):
+def libcl_spdr(rhod, thd, rv, rc, nc, na, sd, dt, aerosol, micro):
     libopts = libcl.lgrngn.opts_t()
-    libopts.cond = libopts.adve = True
+    libopts.cond = True
+    libopts.adve = True
     libopts.coal = libopts.sedi = False
 
-    print "max w qc przed", rv.max()
     micro.step_sync(libopts, thd, rv, rhod)
-    
     micro.step_async(libopts)
-    print "max w qc po", rv.max()
 
-def calc_RH(RH, Temp, rho_d, th_d, rv):
-    for i in range(len(RH)):
+    # absolute number of super-droplets per grid cell
+    micro.diag_sd_conc()
+    sd[:] = np.frombuffer(micro.outbuf())
+    
+    # number of particles (per kg of dry air) with r_w < .5 um
+    micro.diag_wet_rng(0, .5e-6)
+    micro.diag_wet_mom(0)
+    na[:] = np.frombuffer(micro.outbuf())
+
+    # number of particles (per kg of dry air) with r_w > .5 um 
+    micro.diag_wet_rng(.5e-6, 1)
+    micro.diag_wet_mom(0)
+    nc[:] = np.frombuffer(micro.outbuf())
+
+    # cloud water mixing ratio [kg/kg] (same size threshold as above)
+    micro.diag_wet_mom(3)
+    rho_H2O = 1e3
+    rc[:] = 4./3 * math.pi * rho_H2O * np.frombuffer(micro.outbuf())
+
+def calc_S(S, Temp, rho_d, th_d, rv):
+    for i in range(len(S)):
 	Temp[i] = libcl.common.T(th_d[i], rho_d[i])
 	p = libcl.common.p(rho_d[i], rv[i], Temp[i])
 	p_v = rho_d[i] * rv[i] * libcl.common.R_v * Temp[i]
-	RH[i] = p_v / libcl.common.p_vs(Temp[i])
+	S[i] = p_v / libcl.common.p_vs(Temp[i]) - 1
 
 def main(scheme, 
-  nx=300, sl_sg = slice(50,100), crnt=0.1, dt=0.2, nt=5, outfreq=1500,
+  nx=300, sl_sg = slice(50,100), crnt=0.1, dt=0.2, nt=1000, outfreq=100,
   aerosol={
     "meanr":.02e-6, "gstdv":1.4, "n_tot":60e6, 
     # ammonium sulphate aerosol parameters:
     "chem_b":.505, # blk_2m only (sect. 2 in Khvorosyanov & Curry 1999, JGR 104)
     "kappa":.61,    # lgrngn only (CCN-derived value from Table 1 in Petters and Kreidenweis 2007)
-    "sd_conc":64. #TODO trzeba tu?
+    "sd_conc":512. #TODO trzeba tu?
   }
 ):
     th_d = np.ones((nx,))* 303.
@@ -144,18 +164,16 @@ def main(scheme,
     rc = np.zeros((nx,))
     rv[sl_sg] += 8.e-3
     #rc[sl_sg] += 1.e-3 
-    rr = np.zeros((nx,))
     rho_d = np.ones((nx,))*.97
     testowa = np.zeros((nx,))
     testowa[sl_sg]= 1.e4
 
-    RH   = np.empty((nx,))
+    S    = np.empty((nx,))
     Temp = np.empty((nx,))
     
     var_adv = [th_d, rv, testowa]
  
     if scheme in ["1m", "2m"]:
-           rc = np.zeros((nx,))
            rr = np.zeros((nx,))
            var_adv = var_adv + [rc, rr]
 
@@ -166,12 +184,22 @@ def main(scheme,
            var_adv  = var_adv + [nc, nr]
 
     if scheme == "sd":
-        micro = libcl_spdr_init(rho_d, th_d, rv, crnt, dt, aerosol)
+           na = np.zeros((nx,))
+           nc = np.zeros((nx,))
+           rc = np.zeros((nx,))
+           sd = np.zeros((nx,))
+           micro = libcl_spdr_init(rho_d, th_d, rv, crnt, dt, aerosol)
+        
 
-    calc_RH(RH, Temp, rho_d, th_d, rv)
-    dic_var = {"rc":rc, "rv":rv, "th":th_d, "Temp":Temp, "S":RH-1}
-    if   scheme == "2m":
-        dic_var["nc"] =nc
+    calc_S(S, Temp, rho_d, th_d, rv)
+    dic_var = {"rc":rc, "rv":rv, "th":th_d, "Temp":Temp, "S":S}
+
+    if scheme in ["2m","sd"]:
+           dic_var["nc"] = nc
+
+    if scheme == "sd":
+           dic_var["na"] = na
+           dic_var["sd"] = sd
 
     plotting(dic_var, figname="plot_init.pdf", time="init") 
     for it in range(nt):
@@ -190,19 +218,17 @@ def main(scheme,
         elif scheme == "2m":
             libcl_2mom(rho_d, th_d, rv, rc, rr, nc, nr, dt, aerosol)
         elif scheme == "sd":
-            libcl_spdr(rho_d, th_d, rv,                 dt, aerosol, micro)
+            libcl_spdr(rho_d, th_d, rv, rc, nc, na, sd, dt, aerosol, micro)
         else: 
             assert(False)
 
-        calc_RH(RH, Temp, rho_d, th_d, rv) 
+        calc_S(S, Temp, rho_d, th_d, rv) 
                 
         print "testowa po it = ", it
         if it % outfreq == 0 or it in [100]:
-            dic_var = {"rc":rc, "rv":rv, "th":th_d, "Temp":Temp, "S":RH-1}
-            if   scheme == "2m":
-                dic_var["nc"] = nc
             plotting(dic_var, figname="plot_"+str(int(it*dt))+"s.pdf", 
-            time=str(int(it*dt))+"s" )
+              time=str(int(it*dt))+"s" 
+            )
             #plotting(dic_var, figname="plot_"+str(int(it*dt))+"s_ylim.pdf",
             #         time=str(int(it*dt))+"s", ylim_dic={"S":[-0.005, 0.015], "nc":[4.86e7, 4.92e7], "rv":[0.0119,0.0121], "rc":[0.00098, 0.00104]} )
 
