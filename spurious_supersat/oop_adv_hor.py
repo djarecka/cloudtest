86bff5a78f96f47125aae3b04694017768cac247
djarecka <djarecka@gmail.com>
2015-10-30 00:14:23 -0600

    adding n_intrp

--- spurious_supersat/oop_adv_hor.py
+++ spurious_supersat/oop_adv_hor.py
@@ -59,15 +59,17 @@ class Micro:
                                     
 
 class Superdroplet(Micro):
-    def __init__(self, nx, sl_sg, apr, C, dt, nt, sl_act_it, aerosol, setup="rhoconst", dx=2):
+    def __init__(self, nx, sl_sg, apr, C, dt, nt, sl_act_it, aerosol, setup="rhoconst", dx=2, n_intrp=1):
 
         Micro.__init__(self, nx, sl_sg, apr, setup, scheme="sd")
          
-        #TODO czy to sie updatuje??/
+        #TODO czy to sie updatuje??/ nie jak jest n_intrp>1 - pomyslec!
         self.dic_var = dict((k, self.state[k]) for k in ('rc', 'rv', 'th_d', "Temp", "S", "nc"))
                 
         self.aerosol = aerosol
         self.dt = dt
+        self.dx = dx
+        self.n_intrp = n_intrp
         self.nt = nt
         self.sl_act_it = sl_act_it
         self.C = C
@@ -75,8 +77,8 @@ class Superdroplet(Micro):
         
         self.opts_init = libcl.lgrngn.opts_init_t()
         self.opts_init.dt = dt
-        self.opts_init.nx = self.state["rv"].shape[0] #TODO to jest chyba nx??
-        self.opts_init.dx = dx
+        self.opts_init.nx = (self.nx-1) * self.n_intrp + 1
+        self.opts_init.dx = dx / float(self.n_intrp)
         self.opts_init.x1 = self.opts_init.nx * self.opts_init.dx
 
         def lognormal(lnr):
@@ -85,17 +87,37 @@ class Superdroplet(Micro):
                 -(lnr - log(self.aerosol["meanr"]))**2 / 2 / log(self.aerosol["gstdv"])**2
             ) / log(self.aerosol["gstdv"]) / sqrt(2*pi)
                 
-        self.opts_init.sd_conc_mean = self.aerosol["sd_conc"]
+        self.opts_init.sd_conc_mean = self.aerosol["sd_conc"] / self.n_intrp
         self.opts_init.dry_distros = {self.aerosol["kappa"]:lognormal}
         
         self.opts_init.coal_switch = self.opts_init.sedi_switch = False
         
         self.opts_init.sstp_cond = 5
         self.micro = libcl.lgrngn.factory(libcl.lgrngn.backend_t.serial, self.opts_init)
-        
-        self.C_arr = np.ones(self.state["rv"].shape[0]+1) * C
-        self.micro.init(self.state["th_d"], self.state["rv"], self.state["rho_d"], self.C_arr)
 
+        self.state_micro = {}
+        if self.n_intrp == 1:
+            for var in ["rv", "th_d", "rho_d", "sd", "na", "nc", "rc"]:
+                self.state_micro[var] = self.state[var]
+        elif self.n_intrp > 1:
+            self.interp_adv2micro()
+            
+        self.C_arr = np.ones(self.state_micro["rv"].shape[0]+1) * C
+        
+        self.micro.init(self.state_micro["th_d"], self.state_micro["rv"], self.state_micro["rho_d"], self.C_arr)
+        
+    def interp_adv2micro(self):
+        x_adv = np.arange(self.nx)
+        x_micro = np.arange(self.opts_init.nx) / float(self.n_intrp)
+        for var in ["rv", "th_d", "rho_d", "sd", "na", "nc", "rc"]:
+            self.state_micro[var] = np.interp(x_micro, x_adv, self.state[var])
+            
+    def interp_micro2adv(self):
+        x_adv = np.arange(self.nx)
+        x_micro = np.arange(self.opts_init.nx) / float(self.n_intrp)
+        for var in ["rv", "th_d", "rho_d", "sd", "na", "nc", "rc"]:
+            self.state[var] = np.interp(x_adv, x_micro, self.state_micro[var])
+                                                    
 
     def micro_step(self, adve=True):
         libopts = libcl.lgrngn.opts_t()
@@ -103,27 +125,27 @@ class Superdroplet(Micro):
         libopts.adve = adve
         libopts.coal = libopts.sedi = False
         
-        self.micro.step_sync(libopts, self.state["th_d"], self.state["rv"], self.state["rho_d"])
+        self.micro.step_sync(libopts, self.state_micro["th_d"], self.state_micro["rv"], self.state_micro["rho_d"])
         self.micro.step_async(libopts)
         
         # absolute number of super-droplets per grid cell
         self.micro.diag_sd_conc()
-        self.state["sd"][:] = np.frombuffer(self.micro.outbuf())
+        self.state_micro["sd"][:] = np.frombuffer(self.micro.outbuf())
         
         # number of particles (per kg of dry air) with r_w < .5 um
         self.micro.diag_wet_rng(0, .5e-6)
         self.micro.diag_wet_mom(0)
-        self.state["na"][:] = np.frombuffer(self.micro.outbuf())
+        self.state_micro["na"][:] = np.frombuffer(self.micro.outbuf())
         
         # number of particles (per kg of dry air) with r_w > .5 um
         self.micro.diag_wet_rng(.5e-6, 1)
         self.micro.diag_wet_mom(0)
-        self.state["nc"][:] = np.frombuffer(self.micro.outbuf())
+        self.state_micro["nc"][:] = np.frombuffer(self.micro.outbuf())
         
         # cloud water mixing ratio [kg/kg] (same size threshold as above)
         self.micro.diag_wet_mom(3)
         rho_H2O = 1e3
-        self.state["rc"][:] = 4./3 * math.pi * rho_H2O * np.frombuffer(self.micro.outbuf())
+        self.state_micro["rc"][:] = 4./3 * math.pi * rho_H2O * np.frombuffer(self.micro.outbuf())
 
     def slow_act(self):
         self.state["rv"][self.sl_sg] += self.state["rv_sl_act"] / self.sl_act_it
@@ -137,29 +159,33 @@ class Superdroplet(Micro):
         for it in range(self.nt+self.sl_act_it):
             print "it", it
             if it < self.sl_act_it:
-                self.slow_act()
+                self.slow_act()                
                 self.micro_step(adve=False)
             else:
                 if self.apr in ["S_adv", "S_adv_adj"]: self.rv2absS()
                 ss.advection()
                 if self.apr in ["S_adv", "S_adv_adj"]: self.absS2rv()
                 print "po adv rv", self.state["rv"].max()
+                if self.n_intrp > 1: self.interp_adv2micro()
                 self.micro_step()
+                if self.n_intrp > 1: self.interp_micro2adv()
+                #pdb.set_trace()
                 #if apr == "S_adv_adj": self.micro_adj() #TODO dolaczyc metode micro_adjust
             self.calc_S()
-            
-                
-        plotting(self.dic_var, figname="Testplot_init.pdf", time=str(it), )#ylim_dic={"S":[-0.005, 0.015]})
+        #pdb.set_trace()    
+        self.dic_var = dict((k, self.state[k]) for k in ('rc', 'rv', 'th_d', "Temp", "S", "nc"))
+        #pdb.set_trace()
+        plotting(self.dic_var, figname="Testplot_init.pdf", time=str(it), ylim_dic={"S":[-0.005, 0.015]})
         saving_state(self.dic_var, filename="test_dane.txt")#scheme+"_"+apr+"_"+setup+"_"+"data_"+str(int(it*dt))+"s.txt")
         
         
         
-ss  = Superdroplet(nx=300, sl_sg=slice(50,100), apr="trad", C=0.1, dt=0.4, nt=200,
+ss  = Superdroplet(nx=300, sl_sg=slice(50,100), apr="trad", C=0.1, dt=0.4, nt=300,
                     aerosol={
                         "meanr":.02e-6, "gstdv":1.4, "n_tot":1000e6,
                         "chem_b":.505, # blk_2m only (sect. 2 in Khvorosyanov & Curry 1999, JGR 104)
                         "kappa":.61,    # lgrngn only (CCN-derived value from Table 1 in Petters and Kreidenweis 2007)
                         "sd_conc":512 #TODO trzeba tu?
-                        }, sl_act_it=200, setup="slow_act")
+                        }, sl_act_it=200, setup="slow_act", n_intrp=1)
 
 ss.all_sym()
