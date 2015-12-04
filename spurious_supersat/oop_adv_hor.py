d5e551f070b54687c80d7131dfc03aa11fd0e585
djarecka <djarecka@gmail.com>
2015-12-03 22:15:05 -0700

    changing interp., adding fake interp for testing, creating new dictionary for data if test=False

--- spurious_supersat/oop_adv_hor.py
+++ spurious_supersat/oop_adv_hor.py
@@ -9,6 +9,8 @@ import libcloudphxx as libcl
 import numpy as np
 import math
 import json
+import os
+from subprocess import call
 
 from adv_hor import plotting, saving_state
 
@@ -18,7 +20,7 @@ import init_slow_act as sl_act
 import init_default as dft
 
 class Micro:
-    def __init__(self, nx, dx, nt, dt, sl_sg, apr, setup, scheme):
+    def __init__(self, nx, dx, nt, dt, sl_sg, apr, setup, scheme, n_intrp, sl_act_it, test):
         self.nx = nx
         self.dx = dx
         self.nt = nt
@@ -34,6 +36,16 @@ class Micro:
         elif self.setup=="slow_act":            
             self.state, self.var_adv = sl_act.thermo_init(nx=self.nx, sl_sg=self.sl_sg,
                                                           scheme=self.scheme, apr=self.apr)
+        self.test = test    
+        if not self.test:
+            self.outputdir = "output/Test_scheme="+scheme+"_setup="+setup+"_apr="+apr
+            if setup=="slow_act": self.outputdir += "_sl_act_it=" + str(sl_act_it)
+            if n_intrp>1: self.outputdir += "_nintrp=" + str(n_intrp)
+            if os.path.exists(self.outputdir):
+                call(["rm", "-r", self.outputdir])
+            call(["mkdir", self.outputdir])
+        
+            
         
     def calc_S(self):
         for i in range(len(self.state["S"])):
@@ -63,9 +75,9 @@ class Micro:
                                     
 
 class Superdroplet(Micro):
-    def __init__(self, nx, dx, sl_sg, apr, C, dt, nt, sl_act_it, aerosol, setup="rhoconst", n_intrp=1):
+    def __init__(self, nx, dx, sl_sg, apr, C, dt, nt, sl_act_it, aerosol, scheme="sd", setup="rhoconst", n_intrp=1, test=True):
 
-        Micro.__init__(self, nx, dx, nt, dt, sl_sg, apr, setup, scheme="sd")
+        Micro.__init__(self, nx, dx, nt, dt, sl_sg, apr, setup, scheme, n_intrp, sl_act_it, test)
          
         #TODO czy to sie updatuje??/ nie jak jest n_intrp>1 - pomyslec!
         self.dic_var = dict((k, self.state[k]) for k in ('rc', 'rv', 'th_d', "Temp", "S", "nc"))
@@ -104,7 +116,6 @@ class Superdroplet(Micro):
             self.interp_adv2micro()
             
         self.C_arr = np.ones(self.state_micro["rv"].shape[0]+1) * self.C * float(self.n_intrp)
-        #pdb.set_trace()
         self.micro.init(self.state_micro["th_d"], self.state_micro["rv"], self.state_micro["rho_d"], self.C_arr)
         
     def interp_adv2micro(self):
@@ -115,12 +126,28 @@ class Superdroplet(Micro):
             self.state_micro[var] = np.interp(x_micro, x_adv, self.state[var])
             
     def interp_micro2adv(self):
-        x_adv = np.arange(self.nx)
-        x_micro = np.arange(self.opts_init.nx) / float(self.n_intrp)
+        x_adv = np.arange(self.nx) +1 / 2.
+        x_micro = (np.arange(self.opts_init.nx) + 1. /2) / float(self.n_intrp)
         for var in ["rv", "th_d", "rho_d", "sd", "na", "nc", "rc"]:
             self.state[var] = np.interp(x_adv, x_micro, self.state_micro[var])
                                                     
 
+    def fake_interp_adv2micro(self):
+        x_adv = np.arange(self.nx) + 1 / 2.
+        x_micro = (np.arange(self.opts_init.nx) + 1 / 2.) / float(self.n_intrp)
+        #pdb.set_trace()
+        for var in ["rv", "th_d", "rho_d", "sd", "na", "nc", "rc"]:
+            self.state_micro[var] = np.repeat(self.state[var], self.n_intrp)
+            
+    def fake_interp_micro2adv(self):
+        x_adv = np.arange(self.nx) +1 / 2.
+        x_micro = (np.arange(self.opts_init.nx) + 1. /2) / float(self.n_intrp)
+        for var in ["rv", "th_d", "rho_d", "sd", "na", "nc", "rc"]:
+            for i in range(self.state["rv"].shape[0]):
+                self.state[var][i] = self.state_micro[var][i*self.n_intrp:(i+1)*self.n_intrp].mean()
+        
+
+            
     def micro_step(self, adve=True):
         libopts = libcl.lgrngn.opts_t()
         libopts.cond = True
@@ -157,42 +184,47 @@ class Superdroplet(Micro):
         self.calc_S()
         print "po S", self.state["S"].max()
         plotting(self.dic_var, figname="Testplot_init.pdf", time="init")
-
+        if not self.test: saving_state(self.dic_var, filename=os.path.join(self.outputdir, "init.txt"))
+        
         for it in range(self.nt+self.sl_act_it):
             print "it", it
             if it < self.sl_act_it:
-                self.slow_act()                
+                
+                self.slow_act()
+                if self.n_intrp > 1: self.interp_adv2micro()
                 self.micro_step(adve=False)
-                #if it==299: pdb.set_trace()
+                if self.n_intrp > 1: self.interp_micro2adv()
             else:
                 if self.n_intrp > 1: self.interp_adv2micro()
-                if it in [600, 601, 602, 650]: pdb.set_trace()
                 self.micro_step()
-                if it in [600, 601, 602, 650]: pdb.set_trace()
                 if self.n_intrp > 1: self.interp_micro2adv()
                 if self.apr in ["S_adv", "S_adv_adj"]: self.rv2absS()
-                if it==600: pdb.set_trace()
                 ss.advection()
-                if it==600: pdb.set_trace()
                 if self.apr in ["S_adv", "S_adv_adj"]: self.absS2rv()
                             
                 #pdb.set_trace()
                 #if apr == "S_adv_adj": self.micro_adj() #TODO dolaczyc metode micro_adjust
             self.calc_S()
-            if it in [50, 100, 200, 400, 600, 601, 620, 650, 700, 800, 999, 1000, 1200, 1400, 1600, 1800, 1990]:
-
-                self.dic_var = dict((k, self.state[k]) for k in ('rc', 'rv', 'th_d',"na", "S", "nc"))
-                plotting(self.dic_var, figname="newplot_slowit"+str(self.sl_act_it)+"_Crr"+str(self.C)+"_it="+str(it)+".pdf", time=str(it), ylim_dic={"S":[-0.005, 0.015]})
-                saving_state(self.dic_var, filename="test_dane.txt")#scheme+"_"+apr+"_"+setup+"_"+"data_"+str(int(it*dt))+"s.txt")
+            if it in [1, 5, 40, 60, 100, self.sl_act_it/4., self.sl_act_it/2, self.sl_act_it,
+                      self.sl_act_it+10, self.sl_act_it+20, self.sl_act_it+30,
+                      self.sl_act_it+40, self.sl_act_it+50, self.sl_act_it+70,
+                      self.sl_act_it+100, self.sl_act_it+150, self.sl_act_it+200,
+                      self.sl_act_it+250, self.sl_act_it+300, self.sl_act_it+350,
+                      self.sl_act_it+400, self.sl_act_it+450, self.sl_act_it+500,
+                      self.sl_act_it+1000, self.sl_act_it+1500, self.sl_act_it+2000]:
+                self.dic_var = dict((k, self.state[k]) for k in ('rc', 'rv', 'th_d',"na", "nc", "S"))
+                plotting(self.dic_var, figname="plots/newplot_slowit"+str(self.sl_act_it)+"_Crr"+str(self.C)+"_nintrp"+str(self.n_intrp)+"_it="+str(it)+".pdf", time=str(it-self.sl_act_it), ylim_dic={"S":[-0.005, 0.015]})
+                if not self.test: saving_state(self.dic_var, filename=os.path.join(self.outputdir, "it="+str(it-self.sl_act_it)+".txt"))#scheme+"_"+apr+"_"+setup+"_"+"data_"+str(int(it*dt))+"s.txt")
         
         
         
-ss  = Superdroplet(nx=300, dx=2, sl_sg=slice(50,100), apr="trad", C=.1, dt=.1, nt=1400,
+ss  = Superdroplet(nx=320, dx=2, sl_sg=slice(50,100), apr="trad", C=1., dt=.1, nt=10010,
                     aerosol={
                         "meanr":.02e-6, "gstdv":1.4, "n_tot":1e9,
                         "chem_b":.505, # blk_2m only (sect. 2 in Khvorosyanov & Curry 1999, JGR 104)
                         "kappa":.61,    # lgrngn only (CCN-derived value from Table 1 in Petters and Kreidenweis 2007)
-                        "sd_conc":512 #TODO trzeba tu?
-                        }, sl_act_it=600, n_intrp=1, setup="slow_act")
+                        "sd_conc":200 #TODO trzeba tu?
+                        }, sl_act_it=600, n_intrp=5, setup="slow_act", scheme="sd",
+                   test=True)
 
 ss.all_sym()
