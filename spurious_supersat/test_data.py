e6c44934dde777b38136d8f8566c61dd43a9a44f
djarecka <djarecka@gmail.com>
2016-03-11 16:08:34 -0700

    adding tmpdir to tests

--- spurious_supersat/test_data.py
+++ spurious_supersat/test_data.py
@@ -15,18 +15,18 @@ Aerosol={"meanr":.02e-6, "gstdv":1.4, "n_tot":1000e6,
          "kappa":.61,    # lgrngn only (CCN-derived value from Table 1 in Petters and Kreidenweis 2007)
          "sd_conc":128 
 }
-Dt, Nt, It_output_l, Nx, Dx, Sl_sg = 0.2, 51, [50], 300, 2, slice(50,100)
+Dt, Nt, Nx, Dx, Sl_sg = 0.2, 51, 300, 2, slice(50,100)
 
 
 @pytest.mark.parametrize("arg", [
    {"scheme":"sd", "apr":"trad", "setup":"rhoconst", "pl_flag":False, "nx":Nx,
-    "sl_sg":Sl_sg, "crnt":1., "dt":Dt, "nt":Nt, "it_output_l":It_output_l, "aerosol":Aerosol},
+    "sl_sg":Sl_sg, "crnt":1., "dt":Dt, "nt":Nt, "it_output_l":[Nt-1], "aerosol":Aerosol},
    {"scheme":"sd", "apr":"S_adv", "setup":"rhoconst", "pl_flag":False, "nx":Nx,
-    "sl_sg":Sl_sg, "crnt":1., "dt":Dt, "nt":Nt, "it_output_l":It_output_l, "aerosol":Aerosol},
+    "sl_sg":Sl_sg, "crnt":1., "dt":Dt, "nt":Nt, "it_output_l":[Nt-1], "aerosol":Aerosol},
    {"scheme":"sd", "apr":"trad", "setup":"rhoconst", "pl_flag":False, "nx":Nx,
-    "sl_sg":Sl_sg, "crnt":.2, "dt":Dt, "nt":Nt, "it_output_l":It_output_l, "aerosol":Aerosol},
+    "sl_sg":Sl_sg, "crnt":.2, "dt":Dt, "nt":Nt, "it_output_l":[Nt-1], "aerosol":Aerosol},
    {"scheme":"sd", "apr":"S_adv", "setup":"rhoconst", "pl_flag":False, "nx":Nx,
-    "sl_sg":Sl_sg, "crnt":.2, "dt":Dt, "nt":Nt, "it_output_l":It_output_l, "aerosol":Aerosol}
+    "sl_sg":Sl_sg, "crnt":.2, "dt":Dt, "nt":Nt, "it_output_l":[Nt-1], "aerosol":Aerosol}
     
   ])
 def test_data_compare(arg, eps = 0.05):
@@ -48,26 +48,24 @@ def test_data_compare(arg, eps = 0.05):
 @pytest.mark.parametrize("arg", [
     {"nx":Nx, "dx":Dx, "sl_sg":Sl_sg, "apr":"trad", "C":1., "dt":Dt, "time_adv_tot":Nt*Dt,
       "aerosol":Aerosol, "sl_act_time":0, "n_intrp":1, "setup":"rhoconst", "scheme":"sd",
-     "test":False, "it_output_l":It_output_l, "dirname_pre":"pytest"},
+     "test":False, "it_output_l":[Nt-1]},
     {"nx":Nx, "dx":Dx, "sl_sg":Sl_sg, "apr":"S_adv", "C":1., "dt":Dt, "time_adv_tot":Nt*Dt,
      "aerosol":Aerosol, "sl_act_time":0, "n_intrp":1, "setup":"rhoconst", "scheme":"sd",
-     "test":False, "it_output_l":It_output_l, "dirname_pre":"pytest"},
+     "test":False, "it_output_l":[Nt-1]},
     {"nx":Nx, "dx":Dx, "sl_sg":Sl_sg, "apr":"trad", "C":.2, "dt":Dt, "time_adv_tot":Nt*Dt,
      "aerosol":Aerosol, "sl_act_time":0, "n_intrp":1, "setup":"rhoconst", "scheme":"sd",
-     "test":False, "it_output_l":It_output_l, "dirname_pre":"pytest"},
+     "test":False, "it_output_l":[Nt-1]},
     {"nx":Nx, "dx":Dx, "sl_sg":Sl_sg, "apr":"S_adv", "C":.2, "dt":Dt, "time_adv_tot":Nt*Dt,
      "aerosol":Aerosol, "sl_act_time":0, "n_intrp":1, "setup":"rhoconst", "scheme":"sd",
-     "test":False, "it_output_l":It_output_l, "dirname_pre":"pytest"}
+     "test":False, "it_output_l":[Nt-1]}
 ])
-def test_data_compare_oop(arg, eps = 0.05):
+def test_data_compare_oop(tmpdir, arg, eps = 0.05):
     filename_ref = arg["scheme"]+"_"+arg["apr"]+"_"+arg["setup"]+"_C"+str(arg["C"])+"_data_"+str(int((Nt-1)*Dt))+"s.txt"
         
-    dir_name = "output/pytest_scheme=sd_conc="+str(int(Aerosol["sd_conc"]))+"_setup="+arg["setup"]+"_apr="+arg["apr"]+"_dt="+str(Dt)+"_dx="+str(Dx)+"_C="+str(arg["C"]) + "_ntot="+str(int(Aerosol["n_tot"]/1.e6))
-    filename=os.path.join(dir_name, "it="+str(int((Nt-1)*Dt))+"s.txt")
-    ss = oah.Superdroplet(**arg)
+    filename=os.path.join(str(tmpdir), "it="+str(int((Nt-1)*Dt))+"s.txt")
+    ss = oah.Superdroplet(dirname=str(tmpdir), **arg)
     ss.all_sym()
                     
-
     f_test = open(filename)
     data_test = json.load(f_test)
     
