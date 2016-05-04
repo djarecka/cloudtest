0bb75402b8c254a1ca563d49ab922bac91abf9db
djarecka <djarecka@gmail.com>
2016-05-04 17:11:51 -0600

    adding distr (library did not do anything before) and changes to rv,rc in place (so arg dictonary in tests change after calling microphysics)

--- libcloudphxx/libcloudphxx_blk_2m_pytest.py
+++ libcloudphxx/libcloudphxx_blk_2m_pytest.py
@@ -5,6 +5,7 @@ import numpy as np
 from constants_pytest import Rd, Rv, cp, p0 #TODO
 import libcloudphxx as libcl
 import analytic_blk_1m_pytest as an
+import pdb
 
 #TODO: do I need it??
 #def opts_cr(cond = True, cevp = True, revp = True, conv = True,
@@ -19,13 +20,13 @@ import analytic_blk_1m_pytest as an
 #    return opts
 
 #TODO: not needed for cond/evap!!!
-#distr = [{"mean_rd":.04e-6 / 2, "sdev_rd":1.4, "N_stp":60e6, "chem_b":.55},
-#         {"mean_rd":.15e-6 / 2, "sdev_rd":1.6, "N_stp":40e6, "chem_b":.55}]
+distr = [{"mean_rd":.04e-6 / 2, "sdev_rd":1.4, "N_stp":60e6, "chem_b":.55},
+         {"mean_rd":.15e-6 / 2, "sdev_rd":1.6, "N_stp":40e6, "chem_b":.55}]
 
 
 def adj_cellwise(press, T, rv, rc, nc, rr, nr, dt):
     opts = libcl.blk_2m.opts_t()
-#    opts.dry_distros = distr
+    opts.dry_distros = distr
 
     #TODO: should be always zero?
     dot_th = np.array([0.])
@@ -41,5 +42,12 @@ def adj_cellwise(press, T, rv, rc, nc, rr, nr, dt):
     libcl.blk_2m.rhs_cellwise(opts, dot_th, dot_rv, dot_rc, dot_nc, dot_rr, dot_nr,
                               rho_d, th_d, rv, rc, nc, rr, nr, dt)
     print "libcloudphxx_blk_2m, after calling", rv, rc, dot_rc, dot_nc, dot_th
-    return rv + dot_rv*dt, rc + dot_rc*dt, nc + dot_nc*dt, rr + dot_rr*dt, nr + dot_nr*dt
+
+    #pdb.set_trace()
+    rv += dot_rv*dt
+    rc += dot_rc*dt
+    nc += dot_nc*dt
+    rr += dot_rr*dt
+    nr += dot_nr*dt
+    return rv, rc, nc, rr, nr
 
