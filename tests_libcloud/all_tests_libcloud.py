55d764ef6edd7a4b218d8d2e69c07979fc3e5ac6
djarecka <djarecka@gmail.com>
2016-05-04 17:32:22 -0600

    adding tests for 2mom (something is wrong -- doesnt evaporate)

--- tests_libcloud/all_tests_libcloud.py
+++ tests_libcloud/all_tests_libcloud.py
@@ -2,4 +2,5 @@ from subprocess import call
 
 call(["py.test", "test_common_eqs.py"])
 call(["py.test", "--libname=libcloudphxx_blk_1m_pytest", "blk_1mom_test.py"])
+call(["py.test", "--libname=libcloudphxx_blk_2m_pytest", "blk_2mom_test.py"])
 
